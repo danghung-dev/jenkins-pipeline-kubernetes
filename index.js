fabe6d341870b2341eb965a68899f186c59baf23
Hung Dang <hung.dang980@gmail.com>
2019-08-11 23:58:47 +0700

    update code to test re-deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("express")({});
 
 server.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World 222222!");
 });
 console.log("server listen at 3000");
 server.listen(3000);
